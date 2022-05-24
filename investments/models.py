6d141d2a304053fbd3c80c237abba87a80ad8928
Julio Marodin <juliocesar_cocacola@hotmail.com>
2022-05-24 10:50:06 -0300

    feat: investments model

--- investments/models.py
+++ investments/models.py
@@ -1,3 +1,11 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+
+class Investment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    value = models.IntegerField()
+    percentage = models.IntegerField()
+    idea_id = models.ForeignKey("ideas.Idea", on_delete=models.PROTECT)
+    user_id = models.ForeignKey("users.User", on_delete=models.PROTECT)
