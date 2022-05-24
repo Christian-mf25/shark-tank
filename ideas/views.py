a289b40d75f6e85106e5373829bb9e5b77131739
Anderson Silva <klaito.b@gmail.com>
2022-05-24 11:24:31 -0300

    feat: create GET for ideas

--- ideas/views.py
+++ ideas/views.py
@@ -23,36 +23,39 @@ class IdeasView(APIView):
         idea = Idea.objects.create(**serializer.validated_data, limited_date = datetime.now()+timedelta(days=1), user_id = request.user.id, is_activated = activated_idea)
         
         serializer = IdeaSerializer(idea)
-        
+
         return Response(serializer.data)
 
     
     def get(self, _:Request, idea_id=""):
-
         if idea_id:
             idea= Idea.objects.filter(id = idea_id)
             idea.first()
             if not idea:
                 return Response({"error":"Idea is not found"}, status.HTTP_404_NOT_FOUND)
+            if not idea[0].is_activated:
+                return Response({"message":"This proposal is not activated"}, status.HTTP_422_UNPROCESSABLE_ENTITY)
             now = datetime.now()
             if str(now) > str(idea[0].limited_date)[:-6] and idea[0].finished == False:
                 idea.update(amount_collected=0, limited_date = datetime.now()+timedelta(days=1))
-                # investments= Investments.objects.filter(idea_id=idea[0].id)
+                # investments= Investment.objects.filter(idea_id=idea[0].id)
                 # investments.delete()
                
             
             serializer = IdeaSerializer(idea[0])
             return Response(serializer.data, status.HTTP_200_OK)
-
-        ideas = Idea.objects.all()
+               
+        ideas = Idea.objects.filter(is_activated=True).all()
         for ea_idea in ideas:
-            time_now = datetime.now()
+            now = datetime.now()  
             idea = Idea.objects.filter(id = ea_idea.id)
             idea.first()
-            if str(time_now)>str(ea_idea.deadline)[:-6] and not ea_idea.finished:
-                idea.update(amount_collected= 0, deadline = datetime.now()+timedelta(days=1)) 
-                # investments = Investment.objects.filter(idea_id = ea_idea.id)
+            if str(now) > str(ea_idea.limited_date)[:-6] and ea_idea.finished == False:
+                idea.update(amount_collected=0, limited_date= datetime.now()+timedelta(days=1))
+                # investments = Investment.objects.filter(idea_id=ea_idea.id)
                 # investments.delete()
+            
 
-        serializer = IdeaSerializer(ideas, many=True)
+        serializer = IdeaSerializer(ideas, many= True)
+
         return Response(serializer.data, status.HTTP_200_OK)
