ae85845e143ca5633cda6a3d5e7db3ba144a45dd
Julio Marodin <juliocesar_cocacola@hotmail.com>
2022-05-25 17:24:57 -0300

    fix: idea views fixed

--- ideas/models.py
+++ ideas/models.py
@@ -7,11 +7,10 @@ class Idea(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=50)
     value = models.IntegerField()
-    desciption = models.TextField()
+    description = models.TextField()
     amount_collected = models.IntegerField(default=0)
     finished = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(editable=False)
     is_activated = models.BooleanField(default=True)
     user_id = models.ForeignKey("users.User", on_delete=models.PROTECT, related_name="ideas")
-
