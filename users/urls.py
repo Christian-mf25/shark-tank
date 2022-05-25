3571e61c026a812e8ee3fff0d4160a1b10401c1f
Ricardo Oliveira <ricardonegocios700@gmail.com>
2022-05-25 14:35:09 -0300

    feat(user): update, get one and delete ok

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-from .views import UserView, login
+from .views import UserDetailsView, UserView, login
 
-urlpatterns = [path("users/", UserView.as_view()), path("login/", login)]
+urlpatterns = [
+    path("users/", UserView.as_view()),
+    path("users/<uuid>/", UserDetailsView.as_view()),
+    path("login/", login),
+]
