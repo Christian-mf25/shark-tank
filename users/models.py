c8651e4d728c4c2dff9a026f9eb7c3f2a4012774
Christian <christian.mf22@gmail.com>
2022-05-25 12:07:54 -0300

    feat: Create UserManager class

--- users/models.py
+++ users/models.py
@@ -1,8 +1,32 @@
 from uuid import uuid4
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
+
+class UserManager(BaseUserManager):
+    use_in_migrations: bool = True
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("E-mail required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+        
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -13,4 +37,6 @@ class User(AbstractUser):
     is_inv = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["name"]
+
+    objects = UserManager()
