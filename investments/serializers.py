bfc0f5f999f211d967935a7fd675236662e32b6c
Anderson Silva <klaito.b@gmail.com>
2022-05-26 11:30:06 -0300

    fix: change type fields

--- investments/serializers.py
+++ investments/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
+from ideas.serializers import IdeaSerializer
+
+from users.serializers import UserSerializer
 
 
 class InvestmentSerializer(serializers.Serializer):
-    id = serializers.UUIDField(read_only=True)
-    value = serializers.IntegerField()
-    percentage = serializers.IntegerField(read_only=True)
-    idea_id = serializers.UUIDField()
-    user_id = serializers.UUIDField(read_only=True)
+    id=serializers.UUIDField(read_only=True)
+    value=serializers.IntegerField()
+    percentage=serializers.FloatField(read_only=True)
+    idea_id = serializers.UUIDField(write_only=True)
+    user=UserSerializer(read_only=True)
+    idea=IdeaSerializer(read_only=True)
