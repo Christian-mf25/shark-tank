9cea837382b3c38b21b5881c4d00b7c4f6f76464
Anderson Silva <klaito.b@gmail.com>
2022-05-26 11:30:45 -0300

    fix: change POST to update idea amount_collected

--- investments/views.py
+++ investments/views.py
@@ -2,7 +2,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.status import (HTTP_200_OK, HTTP_404_NOT_FOUND,
-                                   HTTP_422_UNPROCESSABLE_ENTITY)
+                                   HTTP_422_UNPROCESSABLE_ENTITY, HTTP_201_CREATED)
 from rest_framework.views import APIView
 
 from ideas.models import Idea
@@ -18,29 +18,28 @@ class InvestmentsView(APIView):
     def post(self, request: Request):
         serializer = InvestmentSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        idea = Idea.objects.filter(id=serializer.idea_id).first()
-
+        idea = Idea.objects.filter(id=request.data["idea_id"])
+       
         if not idea:
             return Response({"error": "idea not found."}, HTTP_404_NOT_FOUND)
+        
+        if idea[0].finished:
+            return Response({"message":"This idea already have finished"}, HTTP_422_UNPROCESSABLE_ENTITY)
 
-        if not idea.is_activated:
+        if not idea[0].is_activated:
             return Response({"error": "idea not active."}, HTTP_422_UNPROCESSABLE_ENTITY)
 
-        collected_plus_investment = serializer["value"] + idea["value"]
-        if collected_plus_investment > idea["value"]:
-            return Response(
-                {"error": f"can not invest more than {idea.value - idea.amount_collected}"},
-                HTTP_422_UNPROCESSABLE_ENTITY,
-            )
+        if request.data["value"]> (idea[0].value - idea[0].amount_collected):
+            return Response({"error":f"Your investment can't be bigger than {idea[0].value-idea[0].amount_collected}"}, HTTP_422_UNPROCESSABLE_ENTITY)
 
-        idea.amount_collected += serializer["value"]
-        idea.save()
+        pr = f'{(request.data["value"]*100/idea[0].value):.1f}'
 
-        serializer["percentage"] = (serializer["value"] * 100) / idea["value"]
-        serializer["user_id"] = request.user.id
+        investment = Investment.objects.create(value = request.data["value"], percentage = pr, user_id = request.user.uuid, idea_id = request.data["idea_id"])
 
-        investment = Investment.objects.create(**serializer.validated_data)
+        idea.update(amount_collected = idea[0].amount_collected + request.data["value"])
+        if idea[0].value == idea[0].amount_collected:
+            idea.update(finished=True, is_activated=False)
 
         serializer = InvestmentSerializer(investment)
 
-        return Response(serializer.data, HTTP_200_OK)
+        return Response(serializer.data, HTTP_201_CREATED)
