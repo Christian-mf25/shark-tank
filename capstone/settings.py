c0189ed82ef1c8e7b7646fe4b24714beb430f194
Anderson Silva <klaito.b@gmail.com>
2022-05-28 10:44:55 -0300

    change DATABASE

--- capstone/settings.py
+++ capstone/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = "capstone.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("NAME_DB"),
-#         "USER": os.getenv("USER_DB"),
-#         "PASSWORD": os.getenv("PASSWORD_DB"),
-#         "HOST": os.getenv("HOST_DB"),
-#         "PORT": os.getenv("PORT_DB"),
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("NAME_DB"),
+        "USER": os.getenv("USER_DB"),
+        "PASSWORD": os.getenv("PASSWORD_DB"),
+        "HOST": os.getenv("HOST_DB"),
+        "PORT": os.getenv("PORT_DB"),
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
